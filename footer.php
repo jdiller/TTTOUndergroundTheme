90071639b03efba800ed84101ebaa5b39118aeaa
= <=>
2011-07-18 03:46:04 +0000

    Added sponsor page link to footer

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
       <div id="footer">
         <img style="padding-bottom:15px" src="<?php bloginfo('template_url')?>/images/freshbooks.gif" height="31" with"76" alt="FreshBooks Logo" title="">
         <span>TechTalksTO is made possible through the generous sponsorship of <a href="http://freshbooks.com">FreshBooks</a></span>
+        <div style="float:right;margin-top:12px;margin-right:5px"><a href="/index.php/sponsors/">Other Sponsors</a></div>
       </div>
     </div>
   </body>
